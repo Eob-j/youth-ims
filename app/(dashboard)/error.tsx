3942ced12be069a1dee72c1c7db64270463773d9
Hadiru Colley <dirudeen22@gmail.com>
2026-03-02 11:28:46 +0000

    feat: Add an error handler handle unexpected errors

---
+++ app/(dashboard)/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button } from "@/components/ui/button";
+// Error boundaries must be Client Components
+
+import { useEffect } from "react";
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
+      <h2>Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
