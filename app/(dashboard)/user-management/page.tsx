dc044fb861685ba610efe1f2c555aa51a2ece50d
Hadiru Colley <dirudeen22@gmail.com>
2026-03-09 12:41:29 +0000

    feat: prevent non admin from accessing page

--- app/(dashboard)/user-management/page.tsx
+++ app/(dashboard)/user-management/page.tsx
@@ -5,6 +5,16 @@ import { UserManagementWrapper } from "./_components/user-management-wrapper";
 export default async function UserManagementPage() {
   const session = await getServerSideSession();
   if (!session) redirect("/login");
+  const isAdmin = session.user.role === "admin";
+
+  if (!isAdmin) {
+    return (
+      <div className="flex w-full h-[75vh] items-center justify-center flex-col">
+        <h3 className="text-destructive text-2xl">Access Denied</h3>
+        <p>You do not have permission to access this page.</p>
+      </div>
+    );
+  }
   const canEditData = canEditDataHelperFn(session.user.role as string);
   return <UserManagementWrapper canEditData={canEditData} />;
 }
